5ceebd187d8701e301706c7f9737ca7614cf3afa
Anton <45329560+Vtorack@users.noreply.github.com>
2020-10-17 12:04:35 +0500

    Add files via upload

---
+++ vsearch/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='cicada',
+    author_email='antomylnikov@gmail.com',
+    url='no_url.com',
+    py_modules=['vsearch'],
+)
